dfa895b280819200f218afc512df482bc6be1114
jkulds <w84my5@yandex.ru>
2023-12-18 06:54:32 +0300

    0.7.4 - fix db for tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config()}
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
