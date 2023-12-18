0c616e677b251794ce9f5006ee18a1455c2c42e0
jkulds <w84my5@yandex.ru>
2023-12-18 06:30:14 +0300

    0.7.2 - update README.md, fix user logic

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-### Hexlet tests and linter status:
-[![Actions Status](https://github.com/jkulds/python-project-52/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/jkulds/python-project-52/actions)
+# ToDo app
+[![Actions Status](https://github.com/jkulds/python-project-52/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/jkulds/python-project-52/actions)[![Maintainability](https://api.codeclimate.com/v1/badges/4bd146d9fa7543bcfcf9/maintainability)](https://codeclimate.com/github/jkulds/python-project-52/maintainability)[![Test Coverage](https://api.codeclimate.com/v1/badges/4bd146d9fa7543bcfcf9/test_coverage)](https://codeclimate.com/github/jkulds/python-project-52/test_coverage)
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.utils.translation import gettext_lazy as _
 
+from task_manager.mixins import UserPermissionMixin, AuthMixin, DeleteOwnMixin
 from task_manager.user.forms import UserEditForm
 
 
@@ -12,21 +15,35 @@ class UserListView(ListView):
     context_object_name = 'users'
 
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
     model = User
     template_name = 'form.html'
     form_class = UserEditForm
     success_url = reverse_lazy('login')
+    extra_context = {
+        'btn_text': _('create'),
+        'title': _('create user')
+    }
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(AuthMixin, UserPermissionMixin,
+                     SuccessMessageMixin, UpdateView):
     model = User
     template_name = 'form.html'
     form_class = UserEditForm
     success_url = reverse_lazy('index')
+    extra_context = {
+        'btn_text': _('update'),
+        'title': _('update user')
+    }
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(AuthMixin, UserPermissionMixin,
+                     DeleteOwnMixin, SuccessMessageMixin, DeleteView):
     model = User
     template_name = 'user/delete.html'
     success_url = reverse_lazy('users')
+    extra_context = {
+        'btn_text': _('delete'),
+        'title': _('delete user')
+    }
