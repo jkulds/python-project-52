3309826775de5d8e6be509a5e19a402a9b15c67a
jkulds <w84my5@yandex.ru>
2023-12-18 09:38:15 +0300

    0.7.9 - ci fixes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jkulds.ru', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'jkulds.ru', 'localhost', 'webserver']
 CSRF_TRUSTED_ORIGINS = [
     'http://jkulds.ru',
     'https://jkulds.ru',
