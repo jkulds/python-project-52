c226f1c6c6cdcba70e35da4bb4b310109ca0f0dd
jkulds <w84my5@yandex.ru>
2023-12-20 13:58:47 +0300

    0.7.9 - ci test fixes

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -6,6 +6,6 @@ from task_manager.user.views import UserListView, UserCreateView, \
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
     path('create/', UserCreateView.as_view(), name='user_create'),
-    path('<int:pk>/update', UserUpdateView.as_view(), name='user_edit'),
-    path('<int:pk>/delete', UserDeleteView.as_view(), name='user_delete'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user_edit'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
