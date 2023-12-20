08de5b827d282b508db18a3fac61c3d449a1cfd8
jkulds <w84my5@yandex.ru>
2023-12-20 16:14:01 +0300

    0.7.9 - ci test fixes

--- task_manager/task/forms.py
+++ task_manager/task/forms.py
@@ -9,10 +9,12 @@ class TaskModelForm(forms.ModelForm):
     name = forms.CharField(
         max_length=150, required=True, label=_('Имя'),
         widget=forms.TextInput(attrs={'placeholder': _('Имя')}),
+        label_suffix=''
     )
     description = forms.CharField(
-        max_length=150, required=True, label=_('Описание'),
+        max_length=150, required=False, label=_('Описание'),
         widget=forms.Textarea(attrs={'placeholder': _('Описание')}),
+        label_suffix=''
     )
 
     class Meta:
